e760d994c2de77ab05955317c089ac6826e76e02
harsh1x4 <harshbsi567@gmail.com>
2022-11-10 16:40:57 +0530

    Probably the final commit

---
+++ Responsive_Navbar/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.more-button').addEventListener('click', function () {
+    document.querySelector('.links').classList.toggle('active');
+    document.querySelector('.toggle-button').classList.toggle('active');
+});
